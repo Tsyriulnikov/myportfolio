825d33f98be1b19f2a7f63a4c82b64cf54ba8817
Tsyriulnikov <max_djvu@mail.ru>
2022-05-26 15:07:02 +0300

    ver. 0.8

--- src/projects/project/Project.js
+++ src/projects/project/Project.js
@@ -8,7 +8,7 @@ const Project = (props) => {
         <div className={style.project}>
             <div className={style.imgContainer}>
                 Картинка
-            <div className={style.linkProject}> Link </div>
+            {/*<div className={style.linkProject}> Link </div>*/}
             </div>
             <h3>{props.title}</h3>
             <span className={style.description}>{props.description}</span>
