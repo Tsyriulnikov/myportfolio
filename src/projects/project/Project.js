5f7051db291e5fdc56286bbc49fea22ca9c9cc30
Tsyriulnikov <max_djvu@mail.ru>
2022-06-05 21:01:37 +0300

    v0.12

--- src/projects/project/Project.js
+++ src/projects/project/Project.js
@@ -6,9 +6,9 @@ import style from "./Project.module.css"
 const Project = (props) => {
     return (
         <div className={style.project}>
-            {/*<div className={style.imgContainer}>*/}
-            {/*    Картинка*/}
-            {/*<div className={style.linkProject}> Link </div>*/}
+            <div className={style.imgContainer}>
+                Картинка
+            <div className={style.linkProject}> Link </div>
             </div>
             <h3>{props.title}</h3>
             <span className={style.description}>{props.description}</span>
