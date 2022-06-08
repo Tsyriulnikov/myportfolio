eb4e0743599a9c292efd0d6159a5d6a8e9acb31b
Tsyriulnikov <max_djvu@mail.ru>
2022-06-08 13:50:45 +0300

    v2.3

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,12 +4,37 @@ import myFoto from '../../assets/myFoto.jpg'
 
 const Home = () => {
     return (
-        <div name='home' className={stylesHome.home}>
+        //     <div name='home' className={stylesHome.home}>
+        //
+        //         <div className={stylesHome.containerText}>
+        //             <p className={stylesHome.helloP}>Привет, меня зовут</p>
+        //             <h1 className={stylesHome.myNameH1}>
+        //                 Максим Цырюльников
+        //             </h1>
+        //             <h2 className={stylesHome.frontEndH2}>
+        //                 Я front-end developer.
+        //             </h2>
+        //             <p className={stylesHome.aboutP}>
+        //                 I’m a full-stack developer specializing in building (and occasionally
+        //                 designing) exceptional digital experiences. Currently, I’m focused on
+        //                 building responsive full-stack web applications.
+        //             </p>
+        //         </div>
+        //
+        //         <div className={stylesHome.containerFoto}>
+        //             <img src={myFoto}/>
+        //         </div>
+        //
+        //     </div>
+        // );
 
+
+        <div name='home' className={stylesHome.mainBlock}>
             <div className={stylesHome.container}>
+                <div className={stylesHome.textContainer}>
 
-                <div className={stylesHome.containerText}>
                     <p className={stylesHome.helloP}>Привет, меня зовут</p>
+                    {/*<span className={stylesHome.textAbout}>Прривет!</span>*/}
                     <h1 className={stylesHome.myNameH1}>
                         Максим Цырюльников
                     </h1>
@@ -22,15 +47,13 @@ const Home = () => {
                         building responsive full-stack web applications.
                     </p>
                 </div>
-                <div className={stylesHome.containerFoto}>
-                    <img src={myFoto}/>
-                </div>
 
+                <div className={stylesHome.fotoContainer}><img className={stylesHome.foto} src={myFoto}/></div>
             </div>
-
-
         </div>
-    );
+    )
+
+
 };
 
 export default Home;
