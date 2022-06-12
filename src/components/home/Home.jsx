9f26388197e5ba78cf3eda4cda0ec8a9c1b32b7c
Tsyriulnikov <max_djvu@mail.ru>
2022-06-12 17:08:28 +0300

    v 1.4

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -33,18 +33,16 @@ const Home = () => {
             <div className={stylesHome.container}>
                 <div className={stylesHome.textContainer}>
 
-                    <p className={stylesHome.helloP}>Привет, меня зовут</p>
+                    <p className={stylesHome.helloP}>Привет, моё имя</p>
                     {/*<span className={stylesHome.textAbout}>Прривет!</span>*/}
                     <h1 className={stylesHome.myNameH1}>
                         Максим Цырюльников
                     </h1>
                     <h2 className={stylesHome.frontEndH2}>
-                        Я front-end developer.
+                        Я front-end разработчик.
                     </h2>
                     <p className={stylesHome.aboutP}>
-                        I’m a full-stack developer specializing in building (and occasionally
-                        designing) exceptional digital experiences. Currently, I’m focused on
-                        building responsive full-stack web applications.
+                        Занимаюсь разработкой веб-проектов под ключ и их техническим сопровождением
                     </p>
                 </div>
 
