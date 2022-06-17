9c90b5c469766f8764132b899560762ca3fd0ed2
Tsyriulnikov <max_djvu@mail.ru>
2022-06-17 12:52:56 +0300

    v1.1

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -11,19 +11,22 @@ import { HiOutlineMail } from 'react-icons/hi';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 import Logo from '../../assets/TSYRIULNIKOV_logo.png';
 import { Link } from 'react-scroll';
+import stylesNavbar from './Navbar.module.css'
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
   const handleClick = () => setNav(!nav);
 
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+    // <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+     <div className={stylesNavbar.navbarMainBlock}>
       <div>
         <img src={Logo} alt='Logo Image' style={{ width: '50px' }} />
       </div>
 
       {/* menu */}
-      <ul className='hidden md:flex'>
+      {/*<ul className='hidden md:flex'>*/}
+      <ul className={stylesNavbar.menu}>
         <li>
           <Link to='home' smooth={true} duration={500}>
             Home
@@ -52,7 +55,8 @@ const Navbar = () => {
       </ul>
 
       {/* Hamburger */}
-      <div onClick={handleClick} className='md:hidden z-10'>
+      {/*<div onClick={handleClick} className='md:hidden z-10'>*/}
+      <div onClick={handleClick} className={stylesNavbar.hamburger}>
         {!nav ? <FaBars /> : <FaTimes />}
       </div>
 
@@ -60,35 +64,40 @@ const Navbar = () => {
       <ul
         className={
           !nav
-            ? 'hidden'
-            : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
+            ? stylesNavbar.hiddenMobileMenu
+            : stylesNavbar.showMobileMenu
         }
       >
-        <li className='py-6 text-4xl'>
+        {/*<li className='py-6 text-4xl'>*/}
+        <li className={stylesNavbar.hamburgerItems}>
           <Link onClick={handleClick} to='home' smooth={true} duration={500}>
             Home
           </Link>
         </li>
-        <li className='py-6 text-4xl'>
-          {' '}
+        {/*<li className='py-6 text-4xl'>*/}
+        <li className={stylesNavbar.hamburgerItems}>
+        {' '}
           <Link onClick={handleClick} to='about' smooth={true} duration={500}>
             About
           </Link>
         </li>
-        <li className='py-6 text-4xl'>
-          {' '}
+        {/*<li className='py-6 text-4xl'>*/}
+        <li className={stylesNavbar.hamburgerItems}>
+        {' '}
           <Link onClick={handleClick} to='skills' smooth={true} duration={500}>
             Skills
           </Link>
         </li>
-        <li className='py-6 text-4xl'>
-          {' '}
+        {/*<li className='py-6 text-4xl'>*/}
+        <li className={stylesNavbar.hamburgerItems}>
+        {' '}
           <Link onClick={handleClick} to='work' smooth={true} duration={500}>
             Work
           </Link>
         </li>
-        <li className='py-6 text-4xl'>
-          {' '}
+        {/*<li className='py-6 text-4xl'>*/}
+        <li className={stylesNavbar.hamburgerItems}>
+        {' '}
           <Link onClick={handleClick} to='contact' smooth={true} duration={500}>
             Contact
           </Link>
