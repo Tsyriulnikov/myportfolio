4c9240a57115985a23931d5833e2cfd45b345c53
Tsyriulnikov <max_djvu@mail.ru>
2022-06-21 19:23:07 +0300

    v.21.06.22.1

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,31 +2,30 @@ import React from 'react';
 import styleAbout from './About.module.css'
 
 const About = () => {
-  return (
-    <div name='about' className={styleAbout.mainBlock}>
-      <div className={styleAbout.container}>
-               <div className={styleAbout.aboutBlockGrid}>
-              <div className={styleAbout.aboutBlockGridMobile}>
-            {/*<p className='text-4xl font-bold inline border-b-4 border-pink-600'>*/}
-            <p className={styleAbout.headerAbout}>
-              Обо мне
-            </p>
-          </div>
-          <div></div>
-          </div>
-          <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
-            <div className='sm:text-right text-4xl font-bold'>
-              <p> Я рад, что вы проявили интерес к моей персоне.</p>
+    return (
+        <div name='about' className={styleAbout.mainBlock}>
+            <div className={styleAbout.container}>
+                <div className={styleAbout.aboutBlockGrid}>
+                    <div className={styleAbout.aboutBlockGridMobile}>
+                        <p className={styleAbout.headerAbout}>
+                            Обо мне
+                        </p>
+                    </div>
+                    <div></div>
+                </div>
+                <div className={styleAbout.story}>
+                    <div className={styleAbout.storyBlock}>
+                        <p> Я рад, что вы проявили интерес к моей персоне.</p>
+                    </div>
+                    <div>
+                        <p>Стараюсь практиковаться и совершенствовать свои навыки, а также
+                            приобретать более глубокие знания в области фронтенд. Очень люблю
+                            свою профессию. Хочу чтобы работа способствовала моему профессиональному росту.</p>
+                    </div>
+                </div>
             </div>
-            <div>
-              <p>Стараюсь практиковаться и совершенствовать свои навыки, а также
-                приобретать более глубокие знания в области фронтенд. Очень люблю
-                свою профессию. Хочу чтобы работа способствовала моему профессиональному росту.</p>
-            </div>
-          </div>
-      </div>
-    </div>
-  );
+        </div>
+    );
 };
 
 export default About;
