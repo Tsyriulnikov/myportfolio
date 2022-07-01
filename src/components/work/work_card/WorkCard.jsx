ee648062098d369d35236ec8405a0e0ce5f082a9
Tsyriulnikov <max_djvu@mail.ru>
2022-07-01 13:11:18 +0300

    v.01.07.22.1

--- src/components/work/work_card/WorkCard.jsx
+++ src/components/work/work_card/WorkCard.jsx
@@ -11,7 +11,8 @@ export const WorkCard=()=>{
             {/* Hover Effects */}
             {/*<div className='opacity-0 group-hover:opacity-100'>*/}
             <div className={stylesWorkCard.hoverDiv}>
-              <span className='text-2xl font-bold text-white tracking-wider'>
+              {/*<span className='text-2xl font-bold text-white tracking-wider'>*/}
+              <span className={stylesWorkCard.captionWork}>
                 React JS Application
               </span>
                 <div className='pt-8 text-center'>
