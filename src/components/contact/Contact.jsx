c4c6c2643684edc503746fcdcb27e3966b9c0079
Tsyriulnikov <max_djvu@mail.ru>
2022-07-02 15:39:52 +0300

    v.02.07.22.1

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -5,14 +5,14 @@ const Contact = () => {
     // <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
     <div name='contact' className={styleContact.mainBlock}>
         <form method='POST' action="https://getform.io/f/6e101a99-f1fe-4c6c-9e93-cf708b10e9e8" className={styleContact.formContact}>
-            <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
-                <p className='text-gray-300 py-4'>// Submit the form below or shoot me an email - max3142255@gmail.com</p>
+            <div className={styleContact.descriptionBlock}>
+                <p className={styleContact.caption}>Contact</p>
+                <p className={styleContact.descrriptionText}>Submit the form below or shoot me an email - max3142255@gmail.com</p>
             </div>
-            <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
-            <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
-            <textarea className='bg-[#ccd6f6] p-2' name="message" rows="10" placeholder='Message'></textarea>
-            <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
+            <input className={styleContact.nameInput} type="text" placeholder='Name' name='name' />
+            <input className={styleContact.emailInput} type="email" placeholder='Email' name='email' />
+            <textarea className={styleContact.messageText} name="message" rows="10" placeholder='Message'></textarea>
+            <button className={styleContact.submitButton}>Let's Collaborate</button>
         </form>
     </div>
   )
