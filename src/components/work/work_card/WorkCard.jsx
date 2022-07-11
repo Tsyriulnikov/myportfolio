7f4f82bd3ac711b60311962e8d4f4a976ec83c84
Tsyriulnikov <max_djvu@mail.ru>
2022-07-11 15:54:35 +0300

    11_07_22.2

--- src/components/work/work_card/WorkCard.jsx
+++ src/components/work/work_card/WorkCard.jsx
@@ -1,21 +1,17 @@
 import React from "react";
 import WorkImg from "../../../assets/workImg.jpeg";
 import stylesWorkCard from './WorkCard.module.css'
-export const WorkCard=()=>{
-    return(
+
+export const WorkCard = () => {
+    return (
         <div
             style={{backgroundImage: `url(${WorkImg})`}}
-            // className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
             className={stylesWorkCard.cardContainer}
         >
-            {/* Hover Effects */}
-            {/*<div className='opacity-0 group-hover:opacity-100'>*/}
             <div className={stylesWorkCard.hoverDiv}>
-              {/*<span className='text-2xl font-bold text-white tracking-wider'>*/}
               <span className={stylesWorkCard.captionWork}>
                 React JS Application
               </span>
-                {/*<div className='pt-8 text-center'>*/}
                 <div className={stylesWorkCard.buttonBlock}>
                     <a href='/'>
                         <button
